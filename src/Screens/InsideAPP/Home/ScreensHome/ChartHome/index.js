17f37ccde5975becfe8524958d0b014413d8aa2c
MyNameIsGustavo <gustavo.rochamaia@gmail.com>
2023-10-17 17:04:33 -0300

    subindo atualizacoes para a aula

--- src/Screens/InsideAPP/Home/ScreensHome/ChartHome/index.js
+++ src/Screens/InsideAPP/Home/ScreensHome/ChartHome/index.js
@@ -1,22 +1,70 @@
 import React, { useContext } from "react";
-import { Image } from "react-native";
+import { Image, View } from "react-native";
 import { ContentContainer, ViewContainer, Title, ContainerImageInitial } from "../../../../../Styles/DefaultStyles/index.js";
 import { AutenticacaoContext } from "../../../../../Contexts/UserContext";
+import { ProgressChart, LineChart } from "react-native-chart-kit";
+import deviceDimensions from "../../../../../Device/DeviceInformation.js";
 
 const ChartHome = () => {
     const { username } = useContext(AutenticacaoContext);
 
+    const desiredHeight = deviceDimensions.height * 0.3;
+    const desiredWidth = deviceDimensions.width * 0.9;
+
+    const data = {
+        labels: ['ED', 'LP', 'SI', 'BD', 'LBD'],
+        data: [0.7, 0.55, 0.3, 0.8, 0.1,],
+    };
+
+    const dataGraficoDois = {
+        labels: ["January", "February", "March", "April", "May", "June"],
+        datasets: [
+            {
+                data: [20, 45, 28, 80, 99, 43],
+                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`, // optional
+                strokeWidth: 6 // optional
+            }
+        ],
+        legend: ["Rainy Days"] // optional
+    };
+
+    const chartConfig = {
+        backgroundGradientFrom: '#106482',
+        backgroundGradientTo: '#106482',
+        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+    };
+
     return (
         <ContentContainer>
             <ViewContainer>
-                <Title>{`Cadastre informações para usar os nossos gráficos, ${username}!`}</Title>
-                <ContainerImageInitial>
-                    <Image
-                        source={require('./Chart-Image.png')}
-                        style={{ width: "100%", height: "50%" }}
-                        resizeMode="cover"
+                <View style={{ alignItems: "center" }}>
+                    <ProgressChart
+                        data={data}
+                        width={desiredWidth}
+                        height={desiredHeight}
+                        chartConfig={chartConfig}
+                        strokeWidth={15}
+                        style={{
+                            borderRadius: 10,
+                            display: 'flex',
+                            justifyContent: 'space-around'
+                        }}
                     />
-                </ContainerImageInitial>
+                </View>
+
+                <View style={{ alignItems: "center" }}>
+                    <LineChart
+                        data={dataGraficoDois}
+                        width={desiredWidth}
+                        height={desiredHeight}
+                        verticalLabelRotation={30}
+                        chartConfig={chartConfig}
+                        style={{
+                            borderRadius: 10,
+                        }}
+                        bezier
+                    />
+                </View>
             </ViewContainer>
         </ContentContainer>
     );
